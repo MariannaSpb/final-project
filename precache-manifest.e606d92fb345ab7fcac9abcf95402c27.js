39314bc5ac2a9dd1be9bc4a4afb1c694cb073b26
Marianna Minich <marry-minich@yandex-team.ru>
2020-05-25 00:22:46 +0300

    Updates

---
+++ precache-manifest.e606d92fb345ab7fcac9abcf95402c27.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822a0b5ec41ff326614583573aa35a10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f66b1b3e3a29a4676b3c",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "247645117d2da1b3411a",
+    "url": "/static/css/main.e39412cb.chunk.css"
+  },
+  {
+    "revision": "f66b1b3e3a29a4676b3c",
+    "url": "/static/js/2.d917cdc8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.d917cdc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247645117d2da1b3411a",
+    "url": "/static/js/main.14c188cd.chunk.js"
+  },
+  {
+    "revision": "8bf9d0197b738935980f",
+    "url": "/static/js/runtime-main.77de9260.js"
+  },
+  {
+    "revision": "e34cef6d60055352c3a966aeada1b1d6",
+    "url": "/static/media/about-bg.e34cef6d.jpg"
+  },
+  {
+    "revision": "b0f8ff56f6eb8aa159ba6150e793daa8",
+    "url": "/static/media/about-bg2.b0f8ff56.jpg"
+  },
+  {
+    "revision": "543763c496b161204cb6e82c5fc2af62",
+    "url": "/static/media/home-bg.543763c4.jpg"
+  },
+  {
+    "revision": "422c742a526a5a9b4f0da72c03668663",
+    "url": "/static/media/logo-1.422c742a.png"
+  }
+]);
